1ff73e43d0c0761f4cf535c2b463d319e4072959
MaJosi-ctrl <mariajosicarvajal@gmail.com>
2025-10-16 17:22:12 -0300

    v3

--- script.js
+++ script.js
@@ -10,13 +10,56 @@ const btn_nova_tarefa = document.querySelector("#btn-nova-tarefa");
 
 const lista_tarefas = document.querySelector("#lista-tarefas");
 
+const audioConcluir = new Audio('sound/gmae.wav');
+
 //--------------------------------------------------------------------------
 // 2. FUNÇÕES DE LÓGICA
 //--------------------------------------------------------------------------
 
 function iniciaToDo() {
-    // Asocia função ao evento de clicar no botão de "Adicionar" nova tarefa
+    // Asocia função "adicionarTarefa()" ao evento de clicar no botão de "Adicionar" nova tarefa
     btn_nova_tarefa.addEventListener("click", adicionarTarefa);
+
+    txt_nova_tarefa.addEventListener("keypress", adicionarTarefaEnter);
+}
+
+function adicionarTarefa() {
+    
+    if (txt_nova_tarefa.value.trim() !== "") {
+        const btn_item = `
+            <div>
+                <button class="btn btn-success btn-sm me-2" onclick="concluirTarefa(this)">Concluir</button>
+                <button class="btn btn-danger btn-sm">Excluir</button>
+            </div>
+        `;
+        
+        const item = document.createElement("li");
+        item.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+        
+        item.innerHTML = "<span class='w-75 text-truncate'>" + txt_nova_tarefa.value + "</span>" + btn_item;
+        
+        lista_tarefas.append(item);
+    }
+    
+    txt_nova_tarefa.value = "";
+    
+    txt_nova_tarefa.focus();
+    
+}
+function adicionarTarefaEnter (evento) {
+    if (evento.key == "Enter") {
+
+    adicionarTarefa();
+    }
+}
+
+function concluirTarefa(elemento) {
+    
+    audioConcluir.play();
+    
+    for (let i = 0; i <= 100; i++) {
+        confetti();
+    }
 }
 
 //--------------------------------------------------------------------------
